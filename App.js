c098b0ac85c8c7f45253c3e61a347432a5f6e85c
Anand Choudhary <anand.choudhary@greenaadhaar.com>
2026-02-22 12:20:44 +0530

    build android

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 
 import NavigationRoot from './navigation';
+import { checkForOTAUpdate } from '../updateService';
 
 export default function App() {
+  useEffect(() => {
+    checkForOTAUpdate();
+  }, []);
   return (
     <>
       <StatusBar style="auto" />
